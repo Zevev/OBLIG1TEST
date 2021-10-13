042153af61a61d9943311babba41e8b6132489d6
Zevev <magnusfa@live.no>
2021-10-13 19:28:34 +0200

    TEst test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String ja;
+        String Nei;
         System.out.println(1800 % 4);
         System.out.println(1800 % 100);
     }
