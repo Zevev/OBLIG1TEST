57b0eb875450e841a68a0144504762926c473535
Zevev <magnusfa@live.no>
2021-10-13 20:03:42 +0200

    Updating workflow

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String ja;
-        String Nei;
+        String Ni;
         System.out.println(1800 % 4);
         System.out.println(1800 % 100);
     }
