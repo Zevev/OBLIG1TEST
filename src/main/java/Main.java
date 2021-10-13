8a1f479daf47fd040550ab7c033d24adc7d96ef2
Zevev <magnusfa@live.no>
2021-10-13 20:24:34 +0200

    Testing github actions showing passed tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String ja;
+        String j;
         String Ni;
         System.out.println(1800 % 4);
         System.out.println(1800 % 100);
